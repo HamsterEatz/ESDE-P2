9f6143ae454f1c154787537d08c12fd01c461665
Nelson <nelsonzhongjunwei@gmail.com>
2021-02-06 11:47:26 +0800

    Updated process submission endpoint

--- SimulatedFrontEnd/public/js/submit_design.js
+++ SimulatedFrontEnd/public/js/submit_design.js
@@ -1,4 +1,4 @@
-let baseUrl = "https://ir2ucxjnni.execute-api.us-east-1.amazonaws.com/prod"
+let baseUrl = "https://86ukurkkmh.execute-api.us-east-1.amazonaws.com/production";
 let $submitDesignFormContainer = $('#submitDesignFormContainer');
 if ($submitDesignFormContainer.length != 0) {
     console.log('Submit design form detected. Binding event handling logic to form elements.');
